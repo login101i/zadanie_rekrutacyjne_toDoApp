8810b6b1c4316087132b146e094ec52e5267ef0d
login101i <maciejkruszyniak@gmail.com>
2022-07-29 21:36:18 +0200

    App.scss styling update

--- src/App.js
+++ src/App.js
@@ -1,92 +1,140 @@
-import React, { Component } from 'react';
-import './App.scss'
+import React, { Component } from "react";
+import "./App.scss";
 
 export default class App extends Component {
+	constructor(props) {
+		super(props);
 
-  constructor(props){
-    super(props);
+		this.state = {
+			items: [],
+			userinput: "",
+			time: setInterval(
+				() => this.setState({ time: new Date().toLocaleTimeString() }),
+				1000
+			),
+			done: [],
+			temperature: 0
+		};
+	}
 
-    this.state = {
-      items: [],
-      userinput: '',
-      time: setInterval(() => this.setState({ time: new Date().toLocaleTimeString() }), 1000),
-      done: [],
-      temperature: 0
-    }
-  }
+	handleChangeInput = (input) => {
+		this.setState({
+			userinput: input
+		});
+	};
 
-  changingInput = (input) => {
-    this.setState({
-      userinput: input
-    })
-  };
+	addToList = (input) => {
+		if (this.state.userinput === "") {
+			alert("empty input");
+		} else {
+			const newitems = this.state.items;
+			newitems.push(input);
+			this.setState({
+				items: newitems,
+				userinput: ""
+			});
+		}
+	};
 
-  addToList = (input) => {
-    if (this.state.userinput === '') {
-      alert('empty input')
-    }
-    else{
-      const newitems = this.state.items;
-      newitems.push(input);
-      this.setState({
-        items: newitems,
-        userinput: ''
-      })
-    }
-  }
+	deleteTask = (indexp) => {
+		const newarray = this.state.items.filter((item, index) => index !== indexp);
+		this.setState({
+			items: newarray
+		});
+	};
 
-  delete = (indexp) => {
-    const newarray = this.state.items.filter((item, index)=> index !== indexp);
-    this.setState({
-      items: newarray
-    })
-  }
+	handleDelete = (indexp) => {
+		const newarray = this.state.done.filter((item, index) => index !== indexp);
+		this.setState({
+			done: newarray
+		});
+	};
 
-  deleteDone = (indexp) => {
-    const newarray = this.state.done.filter((item, index)=> index !== indexp);
-    this.setState({
-      done: newarray
-    });
-  }
+	toDoCompteted = (e) => {
+		const donelist = this.state.done;
+		donelist.push(e);
+		this.setState({
+			done: donelist
+		});
 
-  addToDone = (e) => {
-    const donelist = this.state.done;
-    donelist.push(e)
-    this.setState({
-      done: donelist
-    });
+		if (this.state.items.length === 1) {
+			alert("Good job!", "You clicked the button!", "success");
+		}
+	};
 
-    if (this.state.items.length === 1) {
-      alert("Good job!", "You clicked the button!", "success");
-    }
-  }
+	handleKeyPress = (event) => {
+		if (event.key === "Enter") {
+			this.addToList(this.state.userinput);
+		}
+	};
+	// zmieniona nazwa f
 
-  handleKeyPress = (event, ) => {
-    if (event.key === 'Enter'){
-      this.addToList(this.state.userinput);
-    }
-  }
+	render() {
+		return (
 
-  render() {
-    return <div className='App'>
-      <div className='container'>
-        <h1>{this.state.time}</h1>
-        <div className='userinput'>
-          <input onKeyPress={this.handleKeyPress} placeholder='ENTER TASK'
-                 onChange={(e)=>this.changingInput(e.target.value)} value={this.state.userinput} type='text' />
-          <div className='addbutton' onClick={()=>this.addToList(this.state.userinput)}>ADD</div>
-        </div>
-        <ul>
-          <h1 className='todo'>TO DO</h1>
-          {this.state.items.map((item, index) =>
-              <li onClick={(e)=>this.delete(index)} key={index}>{item}<button className='left' onClick={(e)=>this.addToDone(item)}>DONE</button> </li>)}
-        </ul>
-        <ul>
-          <h1 className='done'>DONE</h1>
-          {this.state.done.map((item, index) =>
-              <li onClick={(e)=>this.deleteDone(index)} key={index}>{item} <span>✔</span></li>)}
-        </ul>
-      </div>
-    </div>
-  }
+  
+			<div className="App">
+				<div className="container">
+					<h1>{this.state.time}</h1>
+					<div className="userInput">
+						<input
+							onKeyPress={this.handleKeyPress}
+							placeholder="ENTER TASK"
+							onChange={(e) => this.handleChangeInput(e.target.value)}
+							value={this.state.userinput}
+							type="text"
+							className="input"
+						/>
+						{this.state.userinput && (
+							<div
+								className="addButton"
+								onClick={() => this.addToList(this.state.userinput)}
+							>
+								ADD
+							</div>
+						)}
+					</div>
+					{this.state.items.length > 0 && (
+						<>
+							<h1 className="todo">TO DO</h1>
+
+							<div className="toDoListContainer">
+								<ul className="toDoList">
+									{this.state.items.map((item, index) => (
+										<>
+											<li onClick={(e) => this.deleteTask(index)} key={index}>
+												{item}
+
+												<button
+													className="todoCompleted"
+													onClick={(e) => this.toDoCompteted(item)}
+												>
+													DONE
+												</button>
+											</li>
+										</>
+									))}
+								</ul>
+							</div>
+						</>
+					)}
+					{this.state.done.length > 0 && <h1 className="done">DONE</h1>}
+
+					{this.state.done.length > 0 && (
+						<>
+							<div className="doneListContainer">
+								<ul className="doneList">
+									{this.state.done.map((item, index) => (
+										<li onClick={(e) => this.handleDelete(index)} key={index}>
+											{item} <span>✔ </span>
+										</li>
+									))}
+								</ul>
+							</div>
+						</>
+					)}
+				</div>
+			</div>
+		);
+	}
 }
